f68510d4d0e411c51a971fadf48f8927c4580b8f
Sondre Namløs Lassen <sondrelassen@gmail.com>
2025-09-16 18:34:45 +0200

    Descriptive commit message

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,3 +7,16 @@
 export const greeting = (firstname: string, lastname: string) => {
     console.log(`Hello ${firstname} ${lastname}`);
 };
+
+const people = [
+    { firstname: "Arne", lastname: "Arnesen" },
+    { firstname: "Magnus", lastname: "Magnusson" },
+    { firstname: "Siri", lastname: "Sirisen" },
+    { firstname: "Stefan", lastname: "Stefanson" },
+];
+
+export const greetTheGang = () => {
+    people.forEach((person) => {
+        greeting(person.firstname, person.lastname);
+    });
+};
