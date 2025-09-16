33e8b23662834c5c07c30e61fc86e7545562c31f
Sondre Namløs Lassen <sondrelassen@gmail.com>
2025-09-16 19:24:54 +0200

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,9 +7,26 @@ const elements = [
     { firstname: 'Guri', lastname: 'Gurisen' },
 ]
 
+<<<<<<< Updated upstream
 let createTable = () => {
     const peopleTable = elements.map(({ firstname, lastname}) => {
         return [firstname, lastname]
     });
     console.table(peopleTable, ["Fornavn", "Etternavn"]);
 }
+=======
+export const greeting = (firstname: string, lastname: string) => {
+    console.log(`Hei hei ${firstname} ${lastname}`);
+};
+
+const people = [
+    { firstname: "Magnus", lastname: "Magnusson" },
+    { firstname: "Stefan", lastname: "Stefanson" },
+];
+
+export const greetTheGang = () => {
+    people.forEach((person) => {
+        greeting(person.firstname, person.lastname);
+    });
+};
+>>>>>>> Stashed changes
